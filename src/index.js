7d3897b3ccfbded3baf583222c9a2c6fed62aee2
leng03 <lengxiong03@gmail.com>
2022-06-17 14:36:47 -0500

    Frontend boiler plate setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+
+const handleAsync = storeAPI => next => action => {
+    if (typeof action === 'function')
+        return action(storeAPI.dispatch, storeAPI.getState)
+
+    next(action)
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose()
+
+const store = createStore(
+    combineReducers({}),
+    composeEnhancers(applyMiddleware(handleAsync)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
