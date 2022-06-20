c4f78941f8d03b9aaff50e30ad8a88605251bc75
jayashreen05 <jayashreen05@gmail.com>
2022-06-20 17:03:17 -0500

    Add Quiz component, quizReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import AddQuiz from "./components/AddQuiz";
+import RecruiterHeader from "./components/RecruiterHeader";
+import {useSelector} from "react-redux";
+import {Container} from "react-bootstrap";
+import GetAllQuizzes from "./components/GetAllQuizzes";
 
-function App() {
-  return<div>
-      team 3 are winners
-    </div>
+export default function App({
+                                _useSelector = useSelector,
+                                RecruiterHeaderC=RecruiterHeader,
+                                GetAllQuizzesC=GetAllQuizzes,
+                                AddQuizC = AddQuiz,
+                            }) {
+    const isAddQuiz = _useSelector(state => state.quizReducer.isAddQuiz)
+    const isGetAllQuiz = _useSelector(state => state.quizReducer.isGetAllQuiz)
+ //   if (isRecruiter) {
+        return <div className={'d-flex justify-content-center'}>
+            <Container>
+                <RecruiterHeaderC/>
+                {isGetAllQuiz && <GetAllQuizzesC/>}
+                {isAddQuiz && <AddQuizC/>}
+{/*                {isEditProc && <EditProcessC/>}*/}
+            </Container>
+        </div>
+//    }
+
 }
-
-export default App;
---
+++ src/components/GetQuiz.js
@@ -0,0 +1,80 @@
+import {useDispatch, useSelector} from "react-redux";
+import {Button, Card, Col, Modal, Table} from "react-bootstrap";
+import {BsFillArchiveFill, BsPencil, BsTrash} from "react-icons/bs";
+
+import {useState} from "react";
+import {EDIT_QUIZ, initiatedeleteQuestion, initiatedeleteQuiz} from "../reducers/quizReducer";
+
+export default function GetQuiz({
+                                       quiz,_initiatedeleteQuiz=initiatedeleteQuiz,_usedispatch=useDispatch,
+                                    _initiatedeleteQuestion=initiatedeleteQuestion
+
+                                   }) {
+    const dispatch = _usedispatch()
+    const [show, setShow] = useState(false);
+    const {
+        quizId, quizQuestion, questionNumber,
+        questionType, quizTemplateId,
+    } = quiz ? quiz : {}
+    console.log(quiz)
+    function handleDeleteQuiz() {
+        dispatch(_initiatedeleteQuiz(quizTemplateId))
+        setShow(false)
+    }
+
+    function handleDeleteQuestion() {
+        dispatch(_initiatedeleteQuestion(quizId))
+    }
+
+
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return     <Col>
+                <div className={'d-flex flex-wrap justify-content-between'}>
+                    <Table striped bordered>
+                        <thead>
+                        <tr>
+                            <th>Quiz Template Id</th>
+                            <th>Question Number</th>
+                            <th>Question Type</th>
+                            <th>Question</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>{quizTemplateId}</td>
+                            <td>{questionNumber}</td>
+                            <td>{questionType}</td>
+                            <td>{quizQuestion}</td>
+                            <td><Button title='Edit' variant="outline-secondary" size='sm' onClick={() => dispatch({type: EDIT_QUIZ,quiz: quiz})}><BsPencil/>
+                            </Button></td>
+                            <td><Button title='Delete Question' variant="outline-danger" size='sm'onClick={handleDeleteQuestion}>
+                                <BsTrash/>
+                            </Button></td>
+
+                            <td><Button title='Delete Quiz' variant="outline-danger" size='sm'   onClick={handleShow}>
+                                <BsFillArchiveFill/>
+                            </Button>
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Delete the entire quiz {quizTemplateId}</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    Are you sure you want to all questions for quiz- {quizTemplateId}?
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant={"outline-secondary"} onClick={handleClose}>Cancel</Button>
+                                    <Button variant={"outline-primary"} onClick={handleDeleteQuiz}>Confirm</Button>
+                                </Modal.Footer>
+                            </Modal></td>
+                        </tr>
+                        </tbody>
+                    </Table>
+
+                </div>
+            </Col>
+
+}
+
