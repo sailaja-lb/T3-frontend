3148f0c46b02f6ae4f7dd962e683ff7501c2fbe6
BradyA862 <brady.abernathey@gmail.com>
2022-06-20 15:10:57 -0500

    added ViewAssigned and reducer for it

--- src/reducers/responseReducer.js
+++ src/reducers/responseReducer.js
@@ -1,14 +1,57 @@
-
+export const GET_QUIZ = 'responseReducer/GET_QUIZ'
+export const GET_QUIZ_START = 'responseReducer/GET_QUIZ'
+export const GET_QUIZ_SUCCESS = 'responseReducer/GET_SUCCESS'
+export const GET_QUIZ_FAILURE = 'responseReducer/GET_QUIZ_FAILURE'
 
 const initialState = {
+    responses: [],
+    quizzes: [],
+    getQuizPending: false,
+    user: {username: 'ssss', password: 'ssss'},
 
 }
 
 export default function responseReducer(state = initialState, action) {
     switch (action?.type) {
 
+        case GET_QUIZ_START:
+            return {
+                ...state,
+                getQuizPending: true
+            }
+
+        case GET_QUIZ_SUCCESS:
+            return {
+                ...state,
+                getQuizPending: false
+            }
+
+        case GET_QUIZ_FAILURE:
+            return {
+                ...state,
+                getQuizPending: false
+            }
+
 
         default:
             return {...state}
     }
+}
+
+export function getAssigned(_fetch = fetch) {
+    return async function getAssignedSE(dispatch) {
+        dispatch({type: GET_QUIZ_START})
+        //TODO get url
+      /*  const url = */
+        const response = await _fetch(url)
+
+        if (response.ok) {
+            const assigned = await response.json()
+            dispatch({type: GET_QUIZ_SUCCESS, payload: assigned})
+        }
+        else {
+            dispatch({type: GET_QUIZ_FAILURE})
+        }
+    }
+
 }
--- src/reducers/responseReducer.test.js
+++ src/reducers/responseReducer.test.js
@@ -1,3 +1,40 @@
-it('should ', function () {
+import responseReducer, {GET_QUIZ_FAILURE, GET_QUIZ_START, GET_QUIZ_SUCCESS} from "./responseReducer";
 
+/** INITIAL STATE **/
+
+it('should start with quizzes empty', function () {
+    const state = responseReducer()
+    expect(state.quizzes.length).toBe(0)
 });
+
+it('should start with responses empty', function () {
+    const state = responseReducer()
+    expect(state.responses.length).toBe(0)
+});
+
+it('should start with getQuizPending false', function () {
+    const state = responseReducer()
+    expect(state.getQuizPending).toBe(false)
+});
+
+/** SWITCH **/
+
+it('should set getQuizPending true on GET_QUIZ_START', function () {
+    const state =  responseReducer(undefined, {type: GET_QUIZ_START})
+    expect(state.getQuizPending).toBe(true)
+});
+
+it('should set getQuizPending false on GET_QUIZ_FAILURE', function () {
+    const initialState = responseReducer()
+    initialState.getQuizPending = true
+    const state = responseReducer(initialState, {type: GET_QUIZ_FAILURE})
+    expect(state.getQuizPending).toBe(false)
+});
+
+it('should set getQuizPending false on GET_QUIZ_SUCCESS', function () {
+    const initialState = responseReducer()
+    initialState.getQuizPending = true
+    const state = responseReducer(initialState, {type: GET_QUIZ_SUCCESS})
+    expect(state.getQuizPending).toBe(false)
+});
+
