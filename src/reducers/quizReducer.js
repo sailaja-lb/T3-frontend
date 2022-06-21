320d1e7ee845d6b8b1a98b514ea7322bea18ffab
leng03 <lengxiong03@gmail.com>
2022-06-21 12:11:34 -0500

    moved quizzes to quiz reducer

--- src/reducers/quizReducer.js
+++ src/reducers/quizReducer.js
@@ -1,7 +1,7 @@
 
 
 const initialState = {
-
+    quizzes: [],
 }
 
 export default function quizReducer(state = initialState, action) {
@@ -10,5 +10,22 @@ export default function quizReducer(state = initialState, action) {
 
         default:
             return {...state}
+    }
+}
+
+export function assignQuiz(_fetch = fetch, assignmentId, quizId) {
+    return async function sideEffect() {
+        const url = `http://localhost:8082/placeholder`
+        const response = await _fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({assignmentId, quizId})
+        })
+        if (response.ok)
+            console.log("passed")
+        else
+            console.log("failed")
     }
 }
