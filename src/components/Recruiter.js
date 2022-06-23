6f26444b0e4d5f419ebe990a629d1db7a9357650
leng03 <lengxiong03@gmail.com>
2022-06-23 10:10:28 -0500

    integrated my work with jay

--- src/components/Recruiter.js
+++ src/components/Recruiter.js
@@ -7,24 +7,29 @@ import AssignQuizToApplicant from "./AssignQuizToApplicant";
 
 import GetCompletedQuizzes from "./GetCompletedQuizzes";
 import EditQuiz from "./QuizFrontend/EditQuiz";
+import GradeCompletedQuizzes from "./GradeCompletedQuizzes";
+
 export default function Recruiter({
-                                _useSelector = useSelector,
-                                RecruiterHeaderC=RecruiterHeader,
-                                GetAllQuizzesC=GetAllQuizzes,
-                                GetCompletedQuizzesC=GetCompletedQuizzes,
-                                EditQuizC=EditQuiz,
-                                AddQuizC = AddQuiz,
-                            }) {
+                                      _useSelector = useSelector,
+                                      RecruiterHeaderC = RecruiterHeader,
+                                      GetAllQuizzesC = GetAllQuizzes,
+                                      GetCompletedQuizzesC = GetCompletedQuizzes,
+                                      EditQuizC = EditQuiz,
+                                      AddQuizC = AddQuiz,
+                                  }) {
     const isAddQuiz = _useSelector(state => state.quizReducer.isAddQuiz)
     const isGetAllQuiz = _useSelector(state => state.quizReducer.isGetAllQuiz)
-    const isGetApplicant= _useSelector(state => state.quizReducer.isGetApplicant)
+    const isGetApplicant = _useSelector(state => state.quizReducer.isGetApplicant)
     const isEditQuiz = _useSelector(state => state.quizReducer.isEditQuiz)
-    if (isGetApplicant){
-        return <div className={'d-flex justify-content-center'}>
-            <GetCompletedQuizzesC/>
-        </div>
-    }
- //   if (isRecruiter) {
+
+    const toggleAssignQuiz = useSelector(state => state.lengReducer.toggleAssignQuiz)
+    const toggleGradeQuiz = useSelector(state => state.lengReducer.toggleGradeQuiz)
+
+    if (toggleAssignQuiz)
+        return <AssignQuizToApplicant/>
+    else if (toggleGradeQuiz)
+        return <GradeCompletedQuizzes/>
+    else
         return <div className={'d-flex justify-content-center'}>
             <Container>
                 <RecruiterHeaderC/>
@@ -33,6 +38,5 @@ export default function Recruiter({
                 {isEditQuiz && <EditQuizC/>}
             </Container>
         </div>
-//    }
 
 }
