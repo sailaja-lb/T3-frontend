c4587fdaaf2d201356ddf2da9334df340f50fb76
sailaja-lb <lakkakulasailaja@gmail.com>
2022-06-24 16:09:50 -0500

    added a button and error messages

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,26 @@
 import {useDispatch, useSelector} from "react-redux";
-import {Button, Card, Form, InputGroup, Row} from "react-bootstrap";
+import {Alert, Button, Card, Form, InputGroup, Row} from "react-bootstrap";
 import {
     ADMIN_CANCEL,
     CANCEL,
     initiateRegister, REGISTER_USER
 } from "../reducers/userReducer";
 import {BsLock, BsPerson} from "react-icons/bs";
+import {useState} from "react";
 
 export default function Register({_useSelector = useSelector, _useDispatch=useDispatch}){
     const users = _useSelector(state => state.userReducer.addNewUser)
-
+    const isAddNewUser = _useSelector(state => state.userReducer.isAddNewUser)
+    const authMessage = _useSelector(state => state.userReducer.authMessage)
+    const failRegisterMessage = _useSelector(state => state.userReducer.failRegisterMessage)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [submit, setSubmit] = useState(false)
     const dispatch = _useDispatch()
 
     function handleRegister(event) {
         event.preventDefault()
+        setSubmit(true)
         dispatch(initiateRegister())
     }
 
@@ -30,17 +37,21 @@ export default function Register({_useSelector = useSelector, _useDispatch=useDi
 
     return <Card style={{borderWidth: 0}}>
         <Card.Body>
+            {failRegisterMessage ? (<Alert variant={"danger"}>
+                Please Enter all the Credentials to Register.</Alert>) : null}
+            {authMessage ? (<Alert variant={"danger"}>
+                Not Authorized. Should be a Admin</Alert>) : null}
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3">
                     <InputGroup className="mb-2">
                         <InputGroup.Text><BsPerson/></InputGroup.Text>
-                        <Form.Control placeholder='Username' onChange={e => updateUsername(e.target.value)}/>
+                        <Form.Control placeholder='Username' onChange={e => updateUsername(e.target.value)} isInvalid={submit && null}/>
                     </InputGroup>
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <InputGroup>
                         <InputGroup.Text><BsLock/></InputGroup.Text>
-                        <Form.Control placeholder='Password' onChange={e => updatePassword(e.target.value)}/>
+                        <Form.Control placeholder='Password' onChange={e => updatePassword(e.target.value)} isInvalid={submit && null}/>
                     </InputGroup>
                 </Form.Group>
                 <Form.Group className="mb-3">
@@ -54,11 +65,12 @@ export default function Register({_useSelector = useSelector, _useDispatch=useDi
                     <Button type={"submit"} variant={"primary"}>Register</Button>
                 </Row>
                 <Row className={'p-3'}>
-                    <Button title='Cancel' variant={"primary"} onClick={() =>  dispatch({type:CANCEL})}>Cancel</Button>
+                    <Button title='Cancel' variant={"primary"} onClick={() =>  dispatch({type:CANCEL})}>Back To Login</Button>
                 </Row>
-                {/*<Row className={'p-3'}>*/}
-                {/*    <Button title='Cancel' variant={"primary"} onClick={() =>  dispatch({type:ADMIN_CANCEL})}>GO BACK TO ADMIN</Button>*/}
-                {/*</Row>*/}
+                {isAddNewUser ?
+                    <Row className={'p-3'}>
+                        <Button title='Cancel' variant={"primary"} onClick={() =>  dispatch({type:ADMIN_CANCEL})}>Back To Admin</Button>
+                    </Row>:null}
             </Form>
         </Card.Body>
     </Card>
